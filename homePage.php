d9b1eda5dee6b63be9206ce190b6ec8add94a802
FuharArora <66731408+FuharArora@users.noreply.github.com>
2021-05-17 12:06:07 +0530

    Add files via upload

---
+++ homePage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="homePage.css">
+    <title> Home Page</title>
+</head>
+<body>
+<centre>
+<div class="bg">
+<h1>WELCOME USER</h1>
+<h2>MAY  YOUR  JOURNEY  WITH  US  BE  AN  INSIGHTFUL , LONG  AND  A  REWARDING  ONE.</h3>
+<input type="button" value="Let's Begin" name="homepage" class="btn">
+</div>
+</centre>
+</body>
+</html> 
+
+
+
+
+
